136919977cb6cf5f3d7b01649e88ebf369f315c1
Seedling <david.r.graham32@gmail.com>
2025-04-09 15:33:40 -0400

    Add new logo svg and favicon. Start 'About'

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h2 className="text-center text-lg font-Ovo mb-2">Introduction</h2>
+      <h3 className="text-center text-5xl font-Ovo">About me</h3>
+      <div></div>
+    </div>
+  );
+};
+
+export default About;
