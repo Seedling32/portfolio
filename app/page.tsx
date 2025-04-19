5d0b792434ea37f48bf49ba7242df1c841adb64a
Seedling <david.r.graham32@gmail.com>
2025-04-19 15:21:53 -0400

    Still fixing mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
-    <div className="relative max-w-screen">
+    <div className="relative max-w-screen overflow-x-hidden">
       <Nav />
       <Intro />
       <About />
